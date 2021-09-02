7004437504aed4b9c7c25d1509e6e8f44ee61456
Nick Rodriguez <nickrrodriguez@gmail.com>
2021-09-01 20:52:26 -0400

    post model added

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 
 # Create your models here.
+STATUS = ( 
+  (0, 'Draft'),
+  (1, 'Publish')
+)
 
+class Post(models.Model): 
+  title = models.CharField(max_length=100, unique=True)
+  author = models.CharField(max_length=100, unique=True)
+  content = models.TextField()
+  create_time = models.DateTimeField(auto_now_add=True)
+  update_time = models.DateField(auto_now=True)
+  
 
-# class Post(models.Model): 
-#   title = models.CharField(max_length=200)
+  class Meta: 
+    ordering = ['-create_time'] 
+
+  def __str__(self): 
+    return self.title 
