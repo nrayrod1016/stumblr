d87bbb0fa56c07b29ab7243165f6d6e593aeb55e
Nick Rodriguez <nickrrodriguez@gmail.com>
2021-09-10 00:11:21 -0400

    style

--- main_app/models.py
+++ main_app/models.py
@@ -39,6 +39,7 @@ class Comment(models.Model):
   create_time = models.DateTimeField(auto_now_add=True)
   active = models.BooleanField(default=False)
  
+  
 
   post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
@@ -51,7 +52,8 @@ class Comment(models.Model):
 class Profile(models.Model): 
  
   user = models.OneToOneField('auth.user',on_delete=models.CASCADE)
-  
+
+
 
   def __str__(self): 
     return self.user.username
